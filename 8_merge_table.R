b000c3e8bb58790a5a37d45e830d4219949430d8
Chikae <65932847+Chikae-Tatsumi@users.noreply.github.com>
2022-03-23 23:44:02 -0400

    Add files via upload

---
+++ 8_merge_table.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(magrittr)
+
+setwd("~/R/Analysis/1_Test/cDNA")
+
+count.table <- read.table("kallisto.isoform.TMM.EXPR.txt",header=T)
+report <- read.csv("trinotate_annotation_report.csv",sep="\t",quote = "",header=T)
+
+count.table$transcript_id <- rownames(count.table)
+report <- report[,-1]
+report.modified <- report %>% distinct(transcript_id, .keep_all = TRUE) 
+
+merge <- merge(count.table, report.modified, by.x='transcript_id', by.y='transcript_id')
+write.csv(merge, "count.table.kallisto.csv")
